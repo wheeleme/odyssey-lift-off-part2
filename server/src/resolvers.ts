c06350c456e972000d8b7a5f97edeff979185961
margaretwheeler <mwheeler1800@gmail.com>
2024-07-03 11:54:55 -0600

    Added context, models, resolvers

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,11 +1,15 @@
-export const resolvers = {
+import { Resolvers } from "./types";
+
+export const resolvers: Resolvers = {
     Query: {
       // get all tracks, will be used to populate the homepage grid of our web client
-      tracksForHome: (_, __, { dataSources }) => {},
+      tracksForHome: (_, __, { dataSources }) => {
+        return dataSources.trackAPI.getTracksForHome();
+      },
     },
     Track: {
-        author: ({ authorId }, _, { dataSources }) => {
+      author: ({ authorId }, _, { dataSources }) => {
         return dataSources.trackAPI.getAuthor(authorId);
-        },
       },
+    },
   };
